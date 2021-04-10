27a0e99a094a68e55940dc84553159d47a8b6333
Lio Young <lio@werewolf.design>
2021-04-10 20:45:58 +0200

    change yiff.rest url to v2.yiff.rest

--- constants.ts
+++ constants.ts
@@ -6,16 +6,16 @@ export default {
     // ! Supported APIs
     "e621": "https://e621.net",
     "e926": "https://e926.net",
-    yiffrest: "https://yiff.rest/V2",
+    yiffrest: "https://v2.yiff.rest",
     floofy: "https://api.floofy.dev",
     shibe: "https://shibe.online/api/",
     fox: "https://randomfox.ca/floof/",
     sheri: "https://sheri.bot/api",
     gelbooru: "https://gelbooru.com",
+    // "r34": "https://rule34.xxx/index.php?page=dapi&s=post&q=index",
 
     // ? Config Stuff
     useragent: `Killswitch/v${pkg.version} (wrwlf.de/killswitch);`,
-    // "r34": "https://rule34.xxx/index.php?page=dapi&s=post&q=index",
     port: process.env.PORT || 3000,
     log: 'killswitch.log',
     error: 'killswitch.error.log',
