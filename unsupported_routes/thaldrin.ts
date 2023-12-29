b131e49e5dc0672dba2314b285701eafb37a4771
Lio <git@lio.cat>
2023-12-29 20:15:54 +0100

    move some routes into an unused folder

---
+++ unsupported_routes/thaldrin.ts
@@ -0,0 +1,41 @@
+// // // @ts-ignore
+// // import { Request, Response } from "express";
+// // import Logger from "../utils/logger";
+// // import request from "../utils/request";
+// const paths = [
+//     '/thal',
+//     '/thaldrin',
+// ]
+
+// async function handler(req: Request, res: Response) {
+//     //     // console.log(req.body)
+//     //     // console.log(req.query)
+
+//     //     let endpoint = req.body.endpoint || req.query.endpoint
+//     //     let apikey: string = req.body.apikey || req.query.apikey
+
+
+//     //     let useragent = req.body.useragent
+//     //     let response;
+//     //     try {
+//     //         response = await request("thaldrin", {
+//     //             endpoint,
+//     //             useragent, apikey
+//     //         })
+
+//     //     } catch (error) {
+//     //         Logger.error(error)
+//     //         response = {
+//     //             success: false,
+//     //             error: {
+//     //                 msg: error.message
+//     //             }
+//     //         }
+//     //     }
+//     // res.send(response)
+// }
+
+// export {
+//     paths,
+//     handler
+// }
